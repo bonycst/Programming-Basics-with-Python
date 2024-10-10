2e5708997cf334b628ac88cff52464e98bfaaf7b
bonycst <114672963+bonycst@users.noreply.github.com>
2024-10-10 11:16:17 +0300

    Add files via upload

---
+++ First Steps in Coding - Lab/08. Pet Shop.py
@@ -0,0 +1,6 @@
+number_of_packages_for_dogs = int(input())
+number_of_packages_for_cats = int(input())
+
+total_sum = number_of_packages_for_dogs*2.5 + number_of_packages_for_cats*4
+
+print(f"{total_sum} lv.")
---
+++ First Steps in Coding - Lab/09. Yard Greening.py
@@ -0,0 +1,7 @@
+yard = float(input())
+
+total_price = (yard*7.61)*0.82
+discount = (yard*7.61)*0.18
+
+print(f"The final price is: {total_price} lv.")
+print(f"The discount is: {discount} lv.")
